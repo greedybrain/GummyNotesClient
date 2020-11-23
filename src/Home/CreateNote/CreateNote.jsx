b76b7fd02883e94f9c403a1adae1ef37c7a52cf3
greedybrain <greedybrain18@gmail.com>
2020-11-22 22:53:29 -0500

    created create note component

---
+++ src/Home/CreateNote/CreateNote.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './CreateNote.css'
+
+const CreateNote = () => {
+        return (
+                <div className="new_note_button_wrapper">
+                        <div className="button">
+                                <i className="fas fa-pencil-alt"></i>
+                        </div>
+                </div>
+        )
+}
+
+export default CreateNote
