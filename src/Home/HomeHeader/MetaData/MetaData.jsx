4bcdecc924c7ef991707d6d33412f2b863f20e1d
greedybrain <greedybrain18@gmail.com>
2020-11-22 22:56:26 -0500

    created meta data comp

---
+++ src/Home/HomeHeader/MetaData/MetaData.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './MetaData.css'
+import NoteCount from './NoteCount/NoteCount'
+
+const MetaData = () => {
+        return (
+                <div className="meta_data_wrapper">
+                        <NoteCount />
+                </div>
+        )
+}
+
+export default MetaData
