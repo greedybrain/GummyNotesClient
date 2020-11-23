3a1084035bc537a7167ca8a142fd375058faa63d
greedybrain <greedybrain18@gmail.com>
2020-11-22 22:58:22 -0500

    created note count comp

---
+++ src/Home/HomeHeader/MetaData/NoteCount/NoteCount.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NoteCount.css'
+
+const NoteCount = () => {
+        return (
+                <div className="note_data">
+                        <i className="fas fa-sticky-note"></i>
+                </div>
+        )
+}
+
+export default NoteCount
