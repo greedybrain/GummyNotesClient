a19805ce4742472c66b69e7b2b499cda1f5b1406
greedybrain <greedybrain18@gmail.com>
2020-11-22 22:53:43 -0500

    created homebody component

---
+++ src/Home/HomeBody/HomeBody.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeBody = () => {
+        return (
+                <ul className="notes">
+                        
+                </ul>
+        )
+}
+
+export default HomeBody
