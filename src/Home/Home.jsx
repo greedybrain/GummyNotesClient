d73a582f7e82de3ba075913cb41060c626e7232e
greedybrain <greedybrain18@gmail.com>
2020-11-22 22:52:36 -0500

    imported home header, create note, and navbar

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import CreateNote from './CreateNote/CreateNote';
+import './Home.css'
+import HomeHeader from './HomeHeader/HomeHeader';
+import Navbar from './Navbar/Navbar';
 
 class Home extends Component {
         render() {
                 return (
                         <div className="home_container">
-                                
+                                <HomeHeader />
+                                <CreateNote />
+                                <Navbar />
                         </div>
                 );
         }
