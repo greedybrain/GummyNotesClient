9d69dd4561124cab1cd611ebb0b607f186260e9d
greedybrain <greedybrain18@gmail.com>
2020-11-22 22:56:39 -0500

    created menu button component

---
+++ src/Home/HomeHeader/MenuButton/MenuButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './MenuButton.css'
+
+const MenuButton = () => {
+        return (
+                <div className="menu_button">
+                        <div className="bar1 bar"></div>
+                        <div className="bar2 bar"></div>
+                        <div className="bar3 bar"></div>
+                </div>
+        )
+}
+
+export default MenuButton
