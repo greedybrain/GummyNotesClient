d518f339b249dcfa8ae9cf24ce6c93391c0f1d2f
greedybrain <greedybrain18@gmail.com>
2020-11-26 13:25:15 -0500

    added Notes comp to HomeBody comp

--- src/Home/HomeBody/HomeBody.jsx
+++ src/Home/HomeBody/HomeBody.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { useState } from 'react'
+import './HomeBody.css'
+import Notes from './Notes/Notes'
+import { notes } from '../../data'
 
 const HomeBody = () => {
+        const [allNotes, setAllNotes] = useState(notes)
         return (
-                <ul className="notes">
-                        
-                </ul>
+                <div className="body">
+                        <Notes allNotes={allNotes} />
+                </div>
         )
 }
 
