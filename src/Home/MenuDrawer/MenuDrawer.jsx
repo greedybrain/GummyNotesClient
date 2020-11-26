7a91817978070198a5f4e7d96faebe2f7ff60a48
greedybrain <greedybrain18@gmail.com>
2020-11-26 13:27:49 -0500

    created and added logic for MenuDrawer comp

---
+++ src/Home/MenuDrawer/MenuDrawer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import MenuBody from './MenuBody/MenuBody'
+import './MenuDrawer.css'
+import MenuFooter from './MenuFooter/MenuFooter'
+import MenuHeader from './MenuHeader/MenuHeader'
+
+const MenuDrawer = ({ toggleMenuDrawer, showMenuDrawer }) => {
+        const classNames = showMenuDrawer ? "animate__slideInLeft" : "animate__slideOutLeft" 
+        return (
+                <div className={`menu_drawer animate__animated animate__faster ${classNames}`}>
+                        <div className="menu">
+                                <MenuHeader toggleMenuDrawer={toggleMenuDrawer} />
+                                <MenuBody />
+                                <MenuFooter />
+                        </div>
+                        <div className="right_overlay animate__animated animate__fadeInLeft"></div>
+                </div>
+        )
+}
+
+export default MenuDrawer
