49b1f9ed51c71f35fa4c075d45029b77c8bbd0cd
greedybrain <greedybrain18@gmail.com>
2020-11-26 13:27:22 -0500

    added toggleMenuDrawer method

--- src/Home/HomeHeader/MenuButton/MenuButton.jsx
+++ src/Home/HomeHeader/MenuButton/MenuButton.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './MenuButton.css'
 
-const MenuButton = () => {
+const MenuButton = ({ toggleMenuDrawer }) => {
         return (
-                <div className="menu_button">
+                <div className="menu_button" onClick={() => toggleMenuDrawer(true)}>
                         <div className="bar1 bar"></div>
                         <div className="bar2 bar"></div>
                         <div className="bar3 bar"></div>
