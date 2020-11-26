99b7f9de844d23c34e7331778e49d7d036f612e1
greedybrain <greedybrain18@gmail.com>
2020-11-26 13:20:04 -0500

    added styling

---
+++ src/Create/Create.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Navbar from '../Home/Navbar/Navbar';
+import './Create.css'
+import CreateForm from './CreateForm/CreateForm';
+import CreateHeader from './CreateHeader/CreateHeader';
+
+class Create extends Component {
+        render() {
+                return (
+                        <div className="create_wrapper">
+                                <CreateHeader history={this.props.history} />
+                                <CreateForm />
+                                <Navbar />
+                        </div>
+                );
+        }
+}
+
+export default Create;
