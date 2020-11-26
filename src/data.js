dc446c070074f87198631f9836329b017bae45bd
greedybrain <greedybrain18@gmail.com>
2020-11-26 13:19:37 -0500

    created dummy file

---
+++ src/data.js
@@ -0,0 +1,57 @@
+export const notes = [
+        {
+                title: 'Note 1',
+                content: 'This is note 1. This note is a reminder that I love this coding stuff',
+                created: '11/23/2020'
+        },
+        {
+                title: 'Note 2',
+                content: 'This is note 2. This note is a reminder that I love this coding stuff',
+                created: '11/23/2020'
+        },
+        {
+                title: 'Note 3',
+                content: 'This is note 3. This note is a reminder that I love this coding stuff',
+                created: '11/23/2020'
+        },
+        {
+                title: 'Note 4',
+                content: 'This is note 4. This note is a reminder that I love this coding stuff',
+                created: '11/23/2020'
+        },
+        {
+                title: 'Note 5',
+                content: 'This is note 5. This note is a reminder that I love this coding stuff',
+                created: '11/23/2020'
+        },
+        {
+                title: 'Note 6',
+                content: 'This is note 6. This note is a reminder that I love this coding stuff',
+                created: '11/23/2020'
+        },
+        {
+                title: 'Note 7',
+                content: 'This is note 7. This note is a reminder that I love this coding stuff',
+                created: '11/23/2020'
+        },
+        {
+                title: 'Note 8',
+                content: 'This is note 8. This note is a reminder that I love this coding stuff',
+                created: '11/23/2020'
+        },
+        {
+                title: 'Note 9',
+                content: 'This is note 9. This note is a reminder that I love this coding stuff',
+                created: '11/23/2020'
+        },
+        {
+                title: 'Note 10',
+                content: 'This is note 10. This note is a reminder that I love this coding stuff',
+                created: '11/23/2020'
+        },
+        {
+                title: 'Note 11',
+                content: 'This is note 11. This note is a reminder that I love this coding stuff',
+                created: '11/23/2020'
+        },
+]
