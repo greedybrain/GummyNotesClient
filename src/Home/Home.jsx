6f86060c7fa9b8714f40ef3dc2b9b65eeb1897d5
greedybrain <greedybrain18@gmail.com>
2020-11-26 13:24:47 -0500

    added menu drawer to Home comp

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import CreateNote from './CreateNote/CreateNote';
 import './Home.css'
+import HomeBody from './HomeBody/HomeBody';
 import HomeHeader from './HomeHeader/HomeHeader';
+import MenuDrawer from './MenuDrawer/MenuDrawer';
 import Navbar from './Navbar/Navbar';
 
 class Home extends Component {
+        state = {
+                showMenuDrawer: false
+        }
+
+        toggleMenuDrawer = shouldOpen => {
+                this.setState({
+                        showMenuDrawer: shouldOpen
+                })
+        }
+
         render() {
                 return (
                         <div className="home_container">
-                                <HomeHeader />
+                                {
+                                        this.state.showMenuDrawer && (
+                                                <MenuDrawer toggleMenuDrawer={this.toggleMenuDrawer} showMenuDrawer={this.state.showMenuDrawer} />
+                                        )
+                                }
+                                <HomeHeader toggleMenuDrawer={this.toggleMenuDrawer} />
+                                <HomeBody />
                                 <CreateNote />
                                 <Navbar />
                         </div>
