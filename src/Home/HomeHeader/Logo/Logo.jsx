d7329cd30c0c4399fefdb220acd0c4ee65780e77
greedybrain <greedybrain18@gmail.com>
2020-11-26 13:27:07 -0500

    wrapped image in navlink

--- src/Home/HomeHeader/Logo/Logo.jsx
+++ src/Home/HomeHeader/Logo/Logo.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './Logo.css'
-import gummyNotesLogo from '../../../Images/gummy-notes-logo.png'
+import gummyNotesLogo from '../../../Images/gummy-notes-logo-2.png'
+import { NavLink } from 'react-router-dom'
 
 const Logo = () => {
         return (
                 <div className="header_logo">
                         <div className="logo_wrapper">
-                                <img src={gummyNotesLogo} alt="logo"/>
+                                <NavLink to="/">
+                                        <img src={gummyNotesLogo} alt="logo"/>
+                                </NavLink>
                         </div>
                 </div>
         )
