1f266e9400321a3c2956403485bc66802dedb4dc
greedybrain <greedybrain18@gmail.com>
2020-11-26 13:28:22 -0500

    created and added logic for MenuFooter comp

---
+++ src/Home/MenuDrawer/MenuFooter/MenuFooter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MenuFooter.css'
+
+const MenuFooter = () => {
+        return (
+                <div className="menu_footer">
+                        <i className="far fa-copyright copy"></i> 2020 Gummy Notes
+                </div>
+        )
+}
+
+export default MenuFooter
