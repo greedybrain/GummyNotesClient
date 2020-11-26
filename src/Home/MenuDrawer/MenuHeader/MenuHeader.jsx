f4b402d5b3ec8f439e01f494f7b1052ee2fc6ac7
greedybrain <greedybrain18@gmail.com>
2020-11-26 13:28:37 -0500

    created and added logic for MenuHeader comp

---
+++ src/Home/MenuDrawer/MenuHeader/MenuHeader.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Logo from '../../HomeHeader/Logo/Logo'
+import './MenuHeader.css'
+import CloseIcon from '@material-ui/icons/Close';
+
+const MenuHeader = ({ toggleMenuDrawer }) => {
+        return (
+                <div className="menu_header">
+                        <div className="close_and_logo">
+                                <Logo />
+                                <div className="close" onClick={() => toggleMenuDrawer(false)}>
+                                        <CloseIcon style={{ color: '#444444', fontSize: '2rem' }} />
+                                </div>
+                        </div>
+                </div>
+        )
+}
+
+export default MenuHeader
