14b4aaef7f5306cf742f53ba515f9340c80afbe7
greedybrain <greedybrain18@gmail.com>
2020-11-26 13:20:42 -0500

    added styling

--- src/Home/CreateNote/CreateNote.jsx
+++ src/Home/CreateNote/CreateNote.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import './CreateNote.css'
 
 const CreateNote = () => {
         return (
                 <div className="new_note_button_wrapper">
-                        <div className="button">
-                                <i className="fas fa-pencil-alt"></i>
-                        </div>
+                        <NavLink to="/create">
+                                <div className="button">
+                                        {/* <i className="fas fa-pencil-alt"></i> */}
+                                        <span role='img' aria-label="pencil">✏️</span>
+                                </div>
+                        </NavLink>
                 </div>
         )
 }
