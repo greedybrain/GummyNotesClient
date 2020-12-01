1b6fbe3ca8b65520693081ddb6d30fc3f1c4eef2
greedybrain <greedybrain18@gmail.com>
2020-11-30 19:59:34 -0500

    added props to menu drawer comp

--- src/Home/MenuDrawer/MenuDrawer.jsx
+++ src/Home/MenuDrawer/MenuDrawer.jsx
@@ -4,13 +4,16 @@ import './MenuDrawer.css'
 import MenuFooter from './MenuFooter/MenuFooter'
 import MenuHeader from './MenuHeader/MenuHeader'
 
-const MenuDrawer = ({ toggleMenuDrawer, showMenuDrawer }) => {
+const MenuDrawer = ({ toggleMenuDrawer, showMenuDrawer, history, dispatchLogout }) => {
         const classNames = showMenuDrawer ? "animate__slideInLeft" : "animate__slideOutLeft" 
         return (
                 <div className={`menu_drawer animate__animated animate__faster ${classNames}`}>
                         <div className="menu">
                                 <MenuHeader toggleMenuDrawer={toggleMenuDrawer} />
-                                <MenuBody />
+                                <MenuBody 
+                                        history={history} 
+                                        dispatchLogout={dispatchLogout}
+                                />
                                 <MenuFooter />
                         </div>
                         <div className="right_overlay animate__animated animate__fadeInLeft"></div>
