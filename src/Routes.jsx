cad21ebb2a4bbc1052500b5a3e84ebd60346be2b
greedybrain <greedybrain18@gmail.com>
2020-11-30 19:58:44 -0500

    added login and signup routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import Create from './Create/Create'
 import Home from './Home/Home'
+import Login from './Login/Login'
 import Search from './Search/Search'
+import Signup from './Signup/Signup'
 
-const Routes = () => {
+const Routes = ({ loggedIn, dispatchLogout, dispatchLogin, dispatchSignup }) => {
         return (
                 <Switch>
                         <Route 
                                 exact
                                 path="/"
-                                render={routerProps => <Home { ...routerProps } />}
+                                render={routerProps => !loggedIn ? <Redirect to='/login' /> : <Home { ...routerProps } loggedIn={loggedIn} dispatchLogout={dispatchLogout} />}
+                        />
+                        <Route 
+                                exact
+                                path="/login"
+                                render={ routerProps => loggedIn ? <Redirect to='/' /> : <Login dispatchLogin={dispatchLogin} {...routerProps} /> }
+                        />
+                        <Route 
+                                exact
+                                path="/signup"
+                                render={ routerProps => loggedIn ? <Redirect to='/' /> : <Signup dispatchSignup={dispatchSignup} {...routerProps} /> }
                         />
                         <Route 
                                 exact
