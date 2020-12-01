545f543a0dfb707822ca4412907b4f979b22b363
greedybrain <greedybrain18@gmail.com>
2020-11-30 20:01:15 -0500

    created signup component

---
+++ src/Signup/Signup.jsx
@@ -0,0 +1,168 @@
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import MenuFooter from '../Home/MenuDrawer/MenuFooter/MenuFooter'
+import LogoTitle from '../Login/LogoTitle/LogoTitle'
+import './Signup.css'
+import EmailIcon from '@material-ui/icons/Email';
+import VisibilityOffSharpIcon from '@material-ui/icons/VisibilityOffSharp';
+import VisibilitySharpIcon from '@material-ui/icons/VisibilitySharp';
+import PersonSharpIcon from '@material-ui/icons/PersonSharp';
+import axios from 'axios'
+
+const Signup = ({ history, dispatchSignup }) => {
+        const [passwordVisible, setPasswordVisible] = useState(false)
+        const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false)
+        const [name, setName] = useState('')
+        const [email, setEmail] = useState('')
+        const [password, setPassword] = useState('')
+        const [confirmPassword, setConfirmPassword] = useState('')
+
+        const handleChange = event => {
+                switch(event.target.name) {
+                        case "signup_name": 
+                                setName(event.target.value)
+                                break;
+                        case "signup_email":
+                                setEmail(event.target.value)
+                                break;
+                        case "signup_password":
+                                setPassword(event.target.value)
+                                break;
+                        case "signup_confirm_password":
+                                setConfirmPassword(event.target.value)
+                                break;
+                        default:
+                                break;
+                }
+        }
+
+        const handlePasswordVisibility = () => {
+                setPasswordVisible(!passwordVisible)
+        }
+
+        const handleConfirmPasswordVisibility = () => {
+                setConfirmPasswordVisible(!confirmPasswordVisible)
+        }
+
+        const handleSubmit = async event => {
+                event.preventDefault()
+                try {
+                        const response = await axios.post(
+                                'http://localhost:5000/api/v1/users',
+                                {
+                                        name,
+                                        email,
+                                        password,
+                                        confirmPassword
+                                },
+                                { withCredentials: true }
+                        )
+                        dispatchSignup(response.data)
+                        history.push('/')
+                        setName('')
+                        setEmail('')
+                        setPassword('')
+                        setConfirmPassword('')
+                } catch (err) {
+                        console.error(err)
+                }
+        }
+
+        return (
+                <div className="signup_page">
+                        <LogoTitle />
+                        <div className="signup_form">
+                                <form onSubmit={handleSubmit}>
+                                        <div className="name">
+                                                <div className="name_icon icon">
+                                                        <PersonSharpIcon style={{ color: "#818181" }} />
+                                                </div>
+                                                <div className="name_field">
+                                                        <input 
+                                                                type="text" 
+                                                                placeholder="Name" 
+                                                                name="signup_name" 
+                                                                value={name}
+                                                                onChange={handleChange} 
+                                                                autoFocus
+                                                                required
+                                                        />
+                                                </div>
+                                        </div>
+                                        <div className="email">
+                                                <div className="email_icon icon">
+                                                        <EmailIcon style={{ color: "#818181" }} />
+                                                </div>
+                                                <div className="email_field">
+                                                        <input 
+                                                                type="email" 
+                                                                placeholder="Email" 
+                                                                name="signup_email" 
+                                                                value={email}
+                                                                onChange={handleChange} 
+                                                                required
+                                                        />
+                                                </div>
+                                        </div>
+                                        <div className="password">
+                                                <div className="password_icon icon" onClick={handlePasswordVisibility}>
+                                                        {
+                                                                passwordVisible 
+                                                                ? <VisibilitySharpIcon style={{ color: "#818181" }} /> 
+                                                                : <VisibilityOffSharpIcon style={{ color: "#818181" }} />
+                                                        }
+                                                </div>
+                                                <div className="password_field">
+                                                        <input 
+                                                                type={
+                                                                        passwordVisible 
+                                                                        ? 'text' 
+                                                                        : 'password'
+                                                                } 
+                                                                placeholder="Password" 
+                                                                name="signup_password" 
+                                                                value={password}
+                                                                onChange={handleChange}
+                                                                required
+                                                        />
+                                                </div>
+                                        </div>
+                                        <div className="confirm">
+                                                <div className="password_icon icon" onClick={handleConfirmPasswordVisibility}>
+                                                        {
+                                                                confirmPasswordVisible 
+                                                                ? <VisibilitySharpIcon style={{ color: "#818181" }} /> 
+                                                                : <VisibilityOffSharpIcon style={{ color: "#818181" }} />
+                                                        }
+                                                </div>
+                                                <div className="confirm_password_field">
+                                                        <input 
+                                                                type={
+                                                                        confirmPasswordVisible 
+                                                                        ? 'text' 
+                                                                        : 'password'
+                                                                } 
+                                                                placeholder="Confirm password" 
+                                                                name="signup_confirm_password" 
+                                                                value={confirmPassword}
+                                                                onChange={handleChange}
+                                                                required
+                                                        />
+                                                </div>
+                                        </div>
+                                        <div className="submit_button">
+                                                <button type="submit">
+                                                        Signup
+                                                </button>
+                                        </div>
+                                </form>
+                        </div>
+                        <div className="or_login">
+                                Already with us?  <NavLink to="/login">Login</NavLink>
+                        </div>
+                        <MenuFooter />
+                </div>
+        )
+}
+
+export default Signup
