bd831a4f9cd3e9fb27212a0452880306cf5c86b6
greedybrain <greedybrain18@gmail.com>
2020-11-30 19:58:06 -0500

    rafacd app comp into class comp

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useEffect, Component } from 'react'
+import Routes from './Routes'
+import axios from 'axios'
+
+class App extends Component {
+  state = {
+    user: {},
+    loggedIn: false
+  }
+
+  getCurrentUser = async () => {
+    try {
+        const response = await axios(
+          'http://localhost:5000/api/v1/users/me',
+          { withCredentials: true },
+        )
+        this.setState({
+          user: response.data,
+          loggedIn: true
+        })
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
+  componentDidMount() {
+    this.getCurrentUser()
+  }
+
+  dispatchSignup = userInfo => {
+    this.setState({
+      user: userInfo,
+      loggedIn: true
+    })
+  }
+
+  dispatchLogin = userInfo => {
+    this.setState({
+      user: userInfo,
+      loggedIn: true
+    })
+  }
+  
+  dispatchLogout = () => {
+    this.setState({
+      loggedIn: false
+    })
+  }
+
+  render() {
+    const { user, loggedIn } = this.state
+    return (
+      <div className="App">
+        <Routes 
+          user={user} 
+          loggedIn={loggedIn} 
+          dispatchSignup={this.dispatchSignup}
+          dispatchLogin={this.dispatchLogin}
+          dispatchLogout={this.dispatchLogout} 
+        />
+      </div>
+    );
+  }
+
+}
+
+export default App;
