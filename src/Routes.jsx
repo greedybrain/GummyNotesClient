e612ae75cb3c4443c1c6347890f1f2d8d8827751
greedybrain <greedybrain18@gmail.com>
2020-11-30 20:20:27 -0500

    ...

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Redirect, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Create from './Create/Create'
 import Home from './Home/Home'
 import Login from './Login/Login'
