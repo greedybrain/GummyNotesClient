26a037fbd9fb5ccd9a457173cee7cd9457ed4814
greedybrain <greedybrain18@gmail.com>
2020-11-30 20:00:38 -0500

    created logo title component

---
+++ src/Login/LogoTitle/LogoTitle.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './LogoTitle.css'
+import logo_title_image from  '../../Images/gummy-notes-logo-title.png'
+import { NavLink } from 'react-router-dom'
+
+const LogoTitle = () => {
+        return (
+                <div className="logo_title">
+                        <div className="logo_title_wrapper">
+                                <NavLink to='/'>
+                                        <img src={logo_title_image} alt="logo title"/>
+                                </NavLink>
+                        </div>
+                </div>
+        )
+}
+
+export default LogoTitle
