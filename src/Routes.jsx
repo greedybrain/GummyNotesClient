c10e0ab5150bec32848e83b49cdf9f03106c9912
greedybrain <greedybrain18@gmail.com>
2020-11-30 20:36:27 -0500

    ...

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -11,12 +11,6 @@ const Routes = ({ loggedIn, dispatchLogout, dispatchLogin, dispatchSignup }) => 
                 <Switch>
                         <Route 
                                 exact
-                                path="/"
-                                // render={routerProps => !loggedIn ? <Redirect to='/login' /> : <Home { ...routerProps } loggedIn={loggedIn} dispatchLogout={dispatchLogout} />}
-                                render={routerProps => <Home { ...routerProps } loggedIn={loggedIn} dispatchLogout={dispatchLogout} />}
-                        />
-                        <Route 
-                                exact
                                 path="/login"
                                 // render={ routerProps => loggedIn ? <Redirect to='/' /> : <Login dispatchLogin={dispatchLogin} {...routerProps} /> }
                                 render={ routerProps => <Login dispatchLogin={dispatchLogin} {...routerProps} /> }
@@ -36,6 +30,12 @@ const Routes = ({ loggedIn, dispatchLogout, dispatchLogin, dispatchSignup }) => 
                                 exact
                                 path="/create"
                                 render={routerProps => <Create { ...routerProps } />}
+                        />
+                        <Route 
+                                exact
+                                path="/home"
+                                // render={routerProps => !loggedIn ? <Redirect to='/login' /> : <Home { ...routerProps } loggedIn={loggedIn} dispatchLogout={dispatchLogout} />}
+                                render={routerProps => <Home { ...routerProps } loggedIn={loggedIn} dispatchLogout={dispatchLogout} />}
                         />
                 </Switch>
         )
