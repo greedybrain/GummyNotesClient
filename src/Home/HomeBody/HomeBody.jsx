f710dc23655f82a0a3cc195db65aa487607d4d4a
greedybrain <greedybrain18@gmail.com>
2020-12-10 12:25:49 -0500

    removed props for context

--- src/Home/HomeBody/HomeBody.jsx
+++ src/Home/HomeBody/HomeBody.jsx
@@ -1,21 +1,12 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+import React from 'react'
 import './HomeBody.css'
 import Notes from './Notes/Notes'
-import { notes } from '../../data'
 
 const HomeBody = () => {
-        const [allNotes, setAllNotes] = useState(notes)
-        const setNotes = () => {
-                setAllNotes(notes)
-        }
 
-        useEffect(() => {
-                setNotes()
-        })
         return (
                 <div className="body">
-                        <Notes allNotes={allNotes} />
+                        <Notes />
                 </div>
         )
 }
