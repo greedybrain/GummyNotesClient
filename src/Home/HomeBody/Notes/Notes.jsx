b668c9a036f27ed72032acec751491d77a426579
greedybrain <greedybrain18@gmail.com>
2020-12-10 12:26:30 -0500

    created edge case logic if user has no notes

--- src/Home/HomeBody/Notes/Notes.jsx
+++ src/Home/HomeBody/Notes/Notes.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Notes.css'
 import Note from './Note/Note'
+import UserContext from '../../../Context/userContext'
 
-const Notes = ({ allNotes, results }) => {
+const Notes = ({ results }) => {
+        const userContext = useContext(UserContext)
+        let { notes } = userContext.state.user
 
-        const notes = results?.length > 0 ? results : allNotes
+        notes = results?.length > 0 ? results : notes
+        notes?.sort((a, b) => b.createdAt.localeCompare(a.createdAt))
         const renderNotes = notes?.map((note, index) => <Note note={note} key={index} />)
         return (
                 <ul className="notes">
-                        { renderNotes }
+                        { 
+                                renderNotes.length === 0 
+                                ? <li>You don't have any notes</li>
+                                : renderNotes
+                        }
                 </ul>
         )
 }
