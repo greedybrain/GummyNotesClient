c16b9db05719f74185adb5d8a025c8d383db8248
greedybrain <greedybrain18@gmail.com>
2020-12-10 12:27:01 -0500

    added userContext

--- src/Home/HomeBody/Notes/Note/Note.jsx
+++ src/Home/HomeBody/Notes/Note/Note.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import moment from 'moment'
+import UserContext from '../../../../Context/userContext'
 import './Note.css'
 
 const Note = ({ note }) => {
+        const userContext = useContext(UserContext)
+        const { dispatchDeleteNote } = userContext.actions
+
+        const handleDelete = e => {
+                if (e.target.children.length > 0) {
+                        dispatchDeleteNote(e.target.dataset.id)
+                } else {
+                        dispatchDeleteNote(e.target.parentElement.dataset.id)
+                }
+        }
+
         return (
-                <li className="note">
-                        <div className="title">
-                                { note.title }
-                        </div>
-                        <div className="content">
-                                { note.content.length > 50 ? `${note.content.slice(0, 47)}...` : note.content }
-                        </div>
-                        <div className="created">
-                                { note.created }
-                        </div>
-                </li>
+                <a className="note" href={'/my/notes/' + note._id}>
+                        <li className="note" style={{ backgroundColor: note.color }} data-id={note._id} onClick={handleDelete}>
+                                <div className="title">
+                                        <span>
+                                                { note.title }
+                                        </span>
+                                </div>
+                                <div className="content">
+                                        { note.content.length > 50 ? `${note.content.slice(0, 47)}...` : note.content }
+                                </div>
+                                <div className="created">
+                                        { 
+                                                moment(note.createdAt).format('MM/D/YY') + ' - ' +
+                                                moment(note?.createdAt).fromNow() 
+                                        }
+                                </div>
+                        </li>
+                </a>
         )
 }
 
